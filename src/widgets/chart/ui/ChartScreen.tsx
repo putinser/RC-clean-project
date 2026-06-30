baa2b9621b582ad9a8475bb987aabc12076d1d81
Stanislav <belyaev.stanislav@balinasoft.com>
2026-06-30 16:30:25 +0300

    safe area for ios

--- src/widgets/chart/ui/ChartScreen.tsx
+++ src/widgets/chart/ui/ChartScreen.tsx
@@ -1,5 +1,10 @@
+import {SafeAreaView} from 'react-native-safe-area-context';
 import {ChartController} from './ChartController';
 
 export const ChartScreen = () => {
-  return <ChartController />;
+  return (
+    <SafeAreaView className="flex-1 bg-background" edges={['top']}>
+      <ChartController />
+    </SafeAreaView>
+  );
 };
